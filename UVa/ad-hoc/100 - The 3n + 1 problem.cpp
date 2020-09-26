3a2271a4270035fa48a1af3f704ed650c7972777
Roopam Jain <56092166+vect-0r@users.noreply.github.com>
2020-09-26 06:38:25 +0530

    Updated 100 - The 3n + 1 problem.cpp
    Added problem link.

--- UVa/ad-hoc/100 - The 3n + 1 problem.cpp
+++ UVa/ad-hoc/100 - The 3n + 1 problem.cpp
@@ -1,3 +1,9 @@
+/*******************************************************************************
+Problem Link:
+https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=36
+********************************************************************************/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
